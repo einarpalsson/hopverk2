4067e0aa361972ef0c024f1815d7b0759ce67c2a
Einar Pálsson <epp5@hi.is>
2020-11-16 19:04:07 +0000

    fært úr index.js í loadvideos

--- src/index.js
+++ src/index.js
@@ -1,40 +1,5 @@
 import { load } from './lib/loadVideos';
 
 document.addEventListener('DOMContentLoaded', async () => {
-  function showData(videos) {
-    console.log('videos', videos)
-    videos.forEach((video) => {
-      console.log('Þetta console log prentast', video);
-      const output = `
-      <div class="col col-4">
-        <div class="card">
-          <div class="mynd thumbnail">
-            <img src="${video.poster}" />
-          </div>
-          <div class"video">
-            <h3 class="card_title_${video.id}">${video.title}</h3>
-            <p class="date_${video.id}">Ehv tímann</p>
-          </div>
-        </div>
-      </div>
-      `;
-      document.getElementById('test').insertAdjacentHTML('afterbegin', output);
-    });
-  }
-
-  load()
-    .then((res) => {
-      if (!res.ok) {
-        throw new Error('None 200 status');
-      } else {
-        res.json()
-          .then((data) => {
-            console.log(data.videos);
-            showData(data.videos);
-          })
-          .catch(() => {
-            const errorMessage = '<p>Óvænt villa</p>';
-          });
-      }
-    });
+  load();
 });
--- src/lib/loadVideos.js
+++ src/lib/loadVideos.js
@@ -1,36 +1,38 @@
-// function makeHtml(data) {
-//   return data.foreach((content) => {
-//     const output = `
-//     <div class="col col-4">
-//       <div class="card">
-//         <div class="mynd thumbnail>
-//           <img src"${content.videos.poster}"
-//         </div>
-//         <div class"content">
-//           <h3 class="card_title_${content.videos.id}">${content.videos.title}</h3>
-//           <p class="date_${content.videos.i}">Ehv tímann</p>
-//         </div>
-//       </div>
-//     </div>
-//     `;
-//     console.log(output);
-//     document.querySelector('.insertion').insertAdjacentHTML('beforeend', output);
-//   })
-// }
+function showData(videos) {
+  console.log('videos', videos)
+  videos.forEach((video) => {
+    console.log('Þetta console log prentast', video);
+    const output = `
+    <div class="col col-4">
+      <div class="card">
+        <div class="mynd thumbnail">
+          <img src="${video.poster}" />
+        </div>
+        <div class"video">
+          <h3 class="card_title_${video.id}">${video.title}</h3>
+          <p class="date_${video.id}">Ehv tímann</p>
+        </div>
+      </div>
+    </div>
+    `;
+    document.getElementById('test').insertAdjacentHTML('afterbegin', output);
+  });
+}
 
 export function load() {
-  return fetch('./videos.json');
-  // .then((res) => {
-  //   if (!res.ok) {
-  //     throw new Error('None 200 status');
-  //   } else {
-  //       res.json()
-  //       .then((data) => {
-  //         })
-  //       .catch(() => {
-  //         const errorMessage = `<p>Óvænt villa</p>`;
-
-  //       })
-  //   }
-  // })
+  return fetch('./videos.json')
+  .then((res) => {
+    if (!res.ok) {
+      throw new Error('None 200 status');
+    } else {
+      res.json()
+        .then((data) => {
+          console.log(data.videos);
+          showData(data.videos);
+        })
+        .catch(() => {
+          const errorMessage = '<p>Óvænt villa</p>';
+        });
+    }
+  });
 }
