2c827d282d5942612d341d0bff2415cba4fec764
Einar Pálsson <epp5@hi.is>
2020-11-16 21:20:16 +0000

    video categories

--- src/lib/loadVideos.js
+++ src/lib/loadVideos.js
@@ -1,21 +1,29 @@
 function showData(videos) {
   console.log('videos', videos)
   videos.forEach((video) => {
-    console.log('Þetta console log prentast', video);
+    const vidID = parseInt(video.id);
     const output = `
     <div class="col col-4">
       <div class="card">
         <div class="mynd thumbnail">
           <img src="${video.poster}" />
         </div>
-        <div class"video">
-          <h3 class="card_title_${video.id}">${video.title}</h3>
+        <div class="video">
+          <a href="#"><h3 class="card_title_${video.id}">${video.title}</h3>
           <p class="date_${video.id}">Ehv tímann</p>
         </div>
       </div>
     </div>
     `;
-    document.getElementById('test').insertAdjacentHTML('afterbegin', output);
+
+    if (vidID <= 3) {
+      document.getElementById('nyleg').insertAdjacentHTML('beforeend', output);
+    }
+
+    if (vidID <= 6) {
+      document.getElementById('teaching').insertAdjacentHTML('beforeend', output);
+      document.getElementById('fun').insertAdjacentHTML('beforeend', output);
+    }
   });
 }
 
