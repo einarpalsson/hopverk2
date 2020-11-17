8b9baa3b0df481e3876e2823c91a1660dcdfa546
Einar Pálsson <epp5@hi.is>
2020-11-17 14:18:42 +0000

    plug

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import { terser } from 'rollup-plugin-terser';
-import json from '@rollup/plugin-json';
 
 module.exports = {
   input: './src/index.js',
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import { load } from './lib/loadVideos';
-import { loadVideoJson } from './lib/time';
 
 document.addEventListener('DOMContentLoaded', async () => {
   load();
-  loadVideoJson();
 });
