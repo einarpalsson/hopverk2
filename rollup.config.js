f718766cc311aa793b8c3898c934cb91875b5c1a
Einar Pálsson <epp5@hi.is>
2020-11-17 14:09:29 +0000

    MERGE

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
+import json from '@rollup/plugin-json';
 
 module.exports = {
   input: './src/index.js',
@@ -24,6 +25,9 @@ module.exports = {
           },
         ],
       ],
+    }),
+    json({
+      compact: true,
     }),
     resolve(),
     commonjs(),
